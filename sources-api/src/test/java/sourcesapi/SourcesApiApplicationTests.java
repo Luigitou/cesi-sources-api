38a78c0465b8828eeba735a2f048148533af5166
Louis <bellefemine.louis@gmail.com>
2022-01-08 12:45:32 +0100

    setup API

---
+++ sources-api/src/test/java/sourcesapi/SourcesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sourcesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
