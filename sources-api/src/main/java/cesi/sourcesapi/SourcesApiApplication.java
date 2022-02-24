6b4bcacc88811274d537e8c0951ae0ad9cc5f11e
Louis <bellefemine.louis@gmail.com>
2022-02-24 16:11:11 +0100

    grant cors for localhost:8080

--- sources-api/src/main/java/cesi/sourcesapi/SourcesApiApplication.java
+++ sources-api/src/main/java/cesi/sourcesapi/SourcesApiApplication.java
@@ -2,9 +2,14 @@ package cesi.sourcesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +22,16 @@ public class SourcesApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SourcesApiApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
