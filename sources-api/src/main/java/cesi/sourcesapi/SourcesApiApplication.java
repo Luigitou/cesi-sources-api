cfd28715d14691bfab55872b3b845953e0047594
Louis <bellefemine.louis@gmail.com>
2022-02-24 14:31:34 +0100

    recreated spring project to clean up docker mess

---
+++ sources-api/src/main/java/cesi/sourcesapi/SourcesApiApplication.java
@@ -0,0 +1,13 @@
+package cesi.sourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcesApiApplication.class, args);
+	}
+
+}
