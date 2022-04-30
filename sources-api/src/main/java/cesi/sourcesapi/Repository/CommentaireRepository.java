b67f5ca5d3fa52f086297d767a96dd62cc19baa4
Edomiyas Temesgen <edomiyas.temesgen@x-pans.com>
2022-04-30 14:24:18 +0200

    Clean up

--- sources-api/src/main/java/cesi/sourcesapi/Repository/CommentaireRepository.java
+++
@@ -1,8 +0,0 @@
-package cesi.sourcesapi.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import cesi.sourcesapi.Model.Commentaire;
-
-public interface CommentaireRepository extends JpaRepository<Commentaire, Integer> {
-
-}
--- sources-api/src/main/java/cesi/sourcesapi/SourcesApiApplication.java
+++ sources-api/src/main/java/cesi/sourcesapi/SourcesApiApplication.java
@@ -1,9 +1,5 @@
 package cesi.sourcesapi;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import cesi.sourcesapi.Model.Privilege;
-import cesi.sourcesapi.Model.Statut;
-import cesi.sourcesapi.Repository.PrivilegeRepository;
-import cesi.sourcesapi.Repository.StatutRepository;
 
 @SpringBootApplication
 public class SourcesApiApplication implements CommandLineRunner{
@@ -38,55 +29,9 @@ public class SourcesApiApplication implements CommandLineRunner{
 			}
 		};
 	}
-	
-	
-	@Autowired
-	private PrivilegeRepository privilegeRepository;
-	
-	@Autowired
-	private StatutRepository statutRepository;
-	
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-				try {
-					List<Statut> listStatut = new ArrayList<Statut>();
-					List<Privilege> listPrivilege = new ArrayList<Privilege>();
-					
-					listPrivilege.addAll(privilegeRepository.findAll());
-					listStatut.addAll(statutRepository.findAll());
-					
-					if (listStatut.isEmpty() && listPrivilege.isEmpty()) {
-						
-						Privilege admin = new Privilege("admin");
-						Privilege superadmin = new Privilege("superadmin");
-						Privilege modo = new Privilege("Moderateur");
-						Privilege user = new Privilege("Utilisateur");
-						
-						Statut adminS = new Statut("admin");
-						Statut superadminS = new Statut("superadmin");
-						Statut modoS = new Statut("Moderateur");
-						Statut userS = new Statut("Utilisateur");
-						
-						adminS.addPrivilege(admin);
-						superadminS.addPrivilege(superadmin);
-						modoS.addPrivilege(modo);
-						userS.addPrivilege(user);
-						
-						statutRepository.save(adminS);
-						statutRepository.save(superadminS);
-						statutRepository.save(modoS);
-						statutRepository.save(userS);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			};
-		};
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		
 	}
 }
